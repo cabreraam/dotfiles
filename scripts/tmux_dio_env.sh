42eb06f58564650189ac97f0e7c5f29744c98f5a
Anthony Cabrera <cabreraam33@gmail.com>
2025-01-08 11:44:34 -0600

    cd to desired directory in each pane

--- scripts/tmux_dio_env.sh
+++ scripts/tmux_dio_env.sh
@@ -6,9 +6,11 @@ tmux new-session -t "dio" \; \
   rename-window "code" \; \
   split-window -h \; \
   send-keys 'conda activate aqumen' C-m \; \
+  send-keys 'cd ~/Data/3_Resources/qci' C-m \; \
   split-window -v \; \
   send-keys 'conda activate aqumen' C-m \; \
-  send-keys 'echo "Hello World"' C-m \; \
+  send-keys 'cd ~/Data/3_Resources/qci' C-m \; \
   neww -n "ipython" \; \
   send-keys 'conda activate aqumen' C-m \; \
+  send-keys 'cd ~/Data/3_Resources/qci' C-m \; \
   select-window -t:@0 \
