2f2782957cb7bf23f0975d5174bb812b09199786
Anthony Cabrera <cabreraam33@gmail.com>
2025-01-08 11:36:14 -0600

    adding a script for setting up a specific tmux environment

---
+++ scripts/tmux_dio_env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+tmux new-session -t "dio" \; \
+  send-keys 'conda activate aqumen' C-m \; \
+  send-keys 'cd ~/Data/3_Resources/qci' C-m \; \
+  rename-window "code" \; \
+  split-window -h \; \
+  send-keys 'conda activate aqumen' C-m \; \
+  split-window -v \; \
+  send-keys 'conda activate aqumen' C-m \; \
+  send-keys 'echo "Hello World"' C-m \; \
+  neww -n "ipython" \; \
+  send-keys 'conda activate aqumen' C-m \; \
+  select-window -t:@0 \
